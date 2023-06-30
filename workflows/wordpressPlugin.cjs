4bf465018c85b174d2ea4ad4cc3a2b88d170545d
Jeremy Castanza <jcastanza@fca.org>
2023-06-30 13:04:36 -0400

    feat: Adds workflow for WordPress plugins
    Signed-off-by: Jeremy Castanza <jcastanza@fca.org>

---
+++ workflows/wordpressPlugin.cjs
@@ -0,0 +1,24 @@
+function wordpressPlugin(pluginFile) {
+	return [
+		['@semantic-release/commit-analyzer', {
+			"releaseRules": [
+				{ "type": "ci", "release": "patch" },
+				{ "type": "docs", "release": "patch" },
+				{ "type": "refactor", "release": "minor" },
+				{ "type": "style", "release": "patch" }
+			]
+		}],
+		'@semantic-release/release-notes-generator',
+		['semantic-release-version-bump', {
+			"files": pluginFile,
+	  }],
+		'@semantic-release/npm',
+		['@semantic-release/git', {
+			"assets": ["package.json", "package-lock.json", pluginFile],
+			"message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+		}],
+		"@semantic-release/github"
+	]
+}
+
+module.exports = wordpressPlugin;
