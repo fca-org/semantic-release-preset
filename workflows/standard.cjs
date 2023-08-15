634bc125689dc11f9cd7383f7753d18b0d078575
Jeremy Castanza <jcastanza@fca.org>
2023-08-15 17:16:40 -0400

    feat: Adds standard workflow
    Signed-off-by: Jeremy Castanza <jcastanza@fca.org>

---
+++ workflows/standard.cjs
@@ -0,0 +1,22 @@
+function standardWorkflow() {
+	return [
+		['@semantic-release/commit-analyzer', {
+			"releaseRules": [
+				{ "type": "ci", "release": "patch" },
+				{ "type": "docs", "release": "patch" },
+				{ "type": "refactor", "release": "minor" },
+				{ "type": "style", "release": "patch" },
+				{ "breaking": true, "release": "major" },
+			]
+		}],
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
+		['@semantic-release/git', {
+			"assets": ["package.json", "package-lock.json"],
+			"message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+		}],
+		"@semantic-release/github"
+	]
+}
+
+module.exports = standardWorkflow;
